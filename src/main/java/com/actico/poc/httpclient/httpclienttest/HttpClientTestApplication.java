8ca3f0a2785e7616b2ac3d2e33c979ef74d41963
Andreas Leibrock <146742322+leibi-actico@users.noreply.github.com>
2024-04-11 13:20:20 +0200

    init

---
+++ src/main/java/com/actico/poc/httpclient/httpclienttest/HttpClientTestApplication.java
@@ -0,0 +1,13 @@
+package com.actico.poc.httpclient.httpclienttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpClientTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpClientTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/actico/poc/httpclient/httpclienttest/HttpClientTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.actico.poc.httpclient.httpclienttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpClientTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
