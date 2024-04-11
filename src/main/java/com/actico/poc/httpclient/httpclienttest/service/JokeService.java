10f46ac8bc0fee79a74c1543d938f5641dc1f81c
Andreas Leibrock <146742322+leibi-actico@users.noreply.github.com>
2024-04-11 14:42:23 +0200

    in parallel

--- src/main/java/com/actico/poc/httpclient/httpclienttest/service/JokeService.java
+++ src/main/java/com/actico/poc/httpclient/httpclienttest/service/JokeService.java
@@ -1,12 +1,19 @@
 package com.actico.poc.httpclient.httpclienttest.service;
 
 import com.actico.poc.httpclient.httpclienttest.model.ExtendedJoke;
+import com.actico.poc.httpclient.httpclienttest.model.Joke;
+import io.micrometer.context.ContextExecutorService;
+import io.micrometer.context.ContextSnapshotFactory;
 import io.micrometer.observation.annotation.Observed;
 import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 @Service
 @RequiredArgsConstructor
@@ -17,13 +24,33 @@ public class JokeService {
     private final OpenfeignService openfeignService;
     private final OpenfeignServiceOkHttp openfeignServiceOkHttp;
 
+
+    private final ExecutorService executor =
+            ContextExecutorService.wrap(
+                    Executors.newVirtualThreadPerTaskExecutor(),
+                    () -> ContextSnapshotFactory.builder().build().captureAll());
+
     public List<ExtendedJoke> getJokes() {
         var jokes = new ArrayList<ExtendedJoke>();
-
-        jokes.add(ExtendedJoke.builder().serviceType("spring http interface").joke(restClientService.getJoke()).build());
-        jokes.add(ExtendedJoke.builder().serviceType("Spring Cloud OpenFeign").joke(openfeignService.getJoke()).build());
-        jokes.add(ExtendedJoke.builder().serviceType("Spring Cloud OpenFeign - okHTTP").joke(openfeignServiceOkHttp.getJoke()).build());
-
+        getCalls().forEach(extendedJokeCall -> {
+            try {
+                jokes.add(ExtendedJoke.builder().serviceType(extendedJokeCall.serviceType).joke(extendedJokeCall.jokeCall.get()).build());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
         return jokes;
     }
+
+    private @NotNull List<ExtendedJokeCall> getCalls() {
+        return List.of(new ExtendedJokeCall("spring http interface",
+                        executor.submit(restClientService::getJoke)),
+                new ExtendedJokeCall("Spring Cloud OpenFeign", executor.submit(openfeignService::getJoke)),
+                new ExtendedJokeCall("Spring Cloud OpenFeign - okHTTP", executor.submit(openfeignServiceOkHttp::getJoke)
+                ));
+    }
+
+    record ExtendedJokeCall(String serviceType, Future<Joke> jokeCall) {
+    }
+
 }
